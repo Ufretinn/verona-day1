2f74fdba3e3faeb816c804a674f951a0d81f2efd
Ufretinn <132437149+Ufretinn@users.noreply.github.com>
2025-11-11 14:07:25 +0100

    FINAL FIX: kill timezone question forever

--- init-db.sh
+++ init-db.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -e
 
+# УБИВАЕМ ВОПРОС ПРО ЧАСОВОЙ ПОЯС НАВСЕГДА
+export LANG=C.UTF-8
+export LC_ALL=C.UTF-8
+export TZ=Europe/Warsaw
+
+# Принудительно создаём кластер с нужными параметрами
+pg_dropcluster --stop 14 main 2>/dev/null || true
+pg_createcluster --start -o "-c lc-collate=C" -o "-c lc-ctype=C" -o "-c locale=C.UTF-8" 14 main
+
+# Создаём пользователя и базу
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE USER verona WITH ENCRYPTED PASSWORD 'SuperSecret123!';
     CREATE DATABASE verona OWNER verona;
@@ -8,3 +18,5 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     \c verona
     CREATE SCHEMA IF NOT EXISTS verona AUTHORIZATION verona;
 EOSQL
+
+echo "Verona DB created with Europe/Warsaw"
